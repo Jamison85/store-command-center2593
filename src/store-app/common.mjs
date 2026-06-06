9a2dbd007493ace145b7aa08e061e9f4e3f14fa7
Jamison85 <jamisonr85@gmail.com>
2026-06-06 11:16:28 -0500

    chore: remove unused rebuild helper

--- src/store-app/common.mjs
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const h = React.createElement;
-
-export function Badge({ text, tone = "neutral" }) {
-  return h("span", { className: `badge badge-${tone}` }, text);
-}
