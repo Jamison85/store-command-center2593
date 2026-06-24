931d6af7d27d53f50945e062973ca5f3b942e932
Jamison85 <jamisonr85@gmail.com>
2026-06-24 06:03:28 -0500

    Set Storechore browser metadata at boot

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from "./App.jsx";
 import "./index.css";
 import "./storechore.css";
 
+document.title = "Storechore";
+document.querySelector('meta[name="theme-color"]')?.setAttribute("content", "#203343");
+
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
